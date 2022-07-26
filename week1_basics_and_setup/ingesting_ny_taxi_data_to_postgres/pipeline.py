9ecb11d78e4eb7b947785b23be85ef0077138aca
Jafa <jafa@Jafas-MacBook-Pro.local>
2022-07-26 08:21:06 +0300

    Added terraform files

---
+++ week1_basics_and_setup/ingesting_ny_taxi_data_to_postgres/pipeline.py
@@ -0,0 +1,12 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+# some fancy job with pandas 
+
+print(f'Job finished successfully for day = f{day}' )
+wq                       
